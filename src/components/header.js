e14c821720669a56c549f31e05b5c0928f371b48
Ishant Lambhate <ishantlambhate@gmail.com>
2018-10-03 09:23:49 +0530

    first commit 1

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Reactnative from 'react-native';
+import {Text} from 'react-native';
+
+const Header=()=>{
+  return <Text>Albums!</Text>;
+}
+export default Header;
